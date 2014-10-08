b78e24c783bf5f65eaea0807cd08a0d9e1ad0915
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-10-08 21:46:34 +0200

    Saving all the stuff before xcode crashes again

--- DATS Finder/DisclaimerViewController.swift
+++ DATS Finder/DisclaimerViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class DisclaimerViewController : UIViewController {
     @IBOutlet weak var understandButton: UIButton!
+    @IBOutlet weak var disclaimerText: UITextView!
     
     override func viewDidLoad() {
-        understandButton.layer.borderColor = UIColor.blueColor().CGColor
+        let blueColor = UIColor(red: 30.0/255, green: 182.0/255, blue: 225.0/255, alpha: 1)
+        understandButton.setTitleColor(blueColor, forState: UIControlState.Normal)
+        understandButton.layer.borderColor = blueColor.CGColor
         understandButton.layer.borderWidth = 2
         understandButton.layer.cornerRadius = 5
+        
+        self.disclaimerText.text = NSLocalizedString("disclaimer", comment:"Some comment");
     }
     
     @IBAction func doUnderstand(sender: AnyObject) {
